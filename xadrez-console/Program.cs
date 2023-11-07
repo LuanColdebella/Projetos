6756a4861be34ffcac99f95f3994572e7c0f7dc2
Luan Coldebella <luanarabuta@gmail.com>
2023-11-07 00:32:20 -0300

    fix ajuste projeto Xadrez

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -8,18 +8,38 @@ try
 
     while (!partida.terminada)
     {
-        Console.Clear();
-        Tela.imprimirTabuleiro(partida.tab);
+        try
+        {
+            Console.Clear();
+            Tela.imprimirTabuleiro(partida.tab);
+            Console.WriteLine();
+            Console.Write("Turno: " + partida.turno);
+            Console.WriteLine();
+            Console.Write("Aguardando jogada: " + partida.jogadorAtual);
+            Console.WriteLine();
 
-        Console.WriteLine();
-        Console.Write("Origem: ");
-        Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+            Console.WriteLine();
+            Console.Write("Origem: ");
+            Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+            partida.validarPosicaoDeorigem(origem);
 
-        Console.Write("Destino: ");
-        Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+            bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
 
-        partida.executaMovimento(origem, destino);
+            Console.Clear();
+            Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
 
+            Console.WriteLine();
+            Console.Write("Destino: ");
+            Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+            partida.validarPosicaoDeDestino(origem, destino);
+
+            partida.realizajogada(origem, destino);
+        }
+        catch (TabuleiroException e)
+        {
+            Console.WriteLine(e.Message);
+            Console.ReadLine();
+        }
     }
 
 }
--- xadrez-console/xadrez/PosicaoXadrez.cs
+++ xadrez-console/xadrez/PosicaoXadrez.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,6 +19,17 @@ namespace xadrez
             this.linha = linha;
         }
 
+        /// <summary>
+        /// LINHA Como linha é igual a 1, a primeira parte da conta será 8 - 1, que é igual a 7. 
+        /// Isso representa o número da linha em uma matriz bidimensional, onde as linhas são numeradas de 0 a 7.
+        /// 
+        ///COLUNA - 'a': Neste caso, coluna é 'c' e 'a' é o caractere 'a'.
+        ///Os valores ASCII de 'c' e 'a' são 99 e 97, respectivamente.Portanto,
+        ///a segunda parte da conta será 99 - 97, que é igual a 2. Isso representa
+        ///o índice da coluna em uma matriz bidimensional, onde as colunas são numeradas de 0 a 7,
+        ///da esquerda para a direita.
+        /// </summary>
+        /// <returns></returns>
         public Posicao toPosicao()
         {
             return new Posicao(8 - linha, coluna - 'a');
